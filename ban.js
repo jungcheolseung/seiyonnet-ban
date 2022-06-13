aa6a738e47d604fb0661babccabfd361b0c3c0d9
Cheolseung Jung <jungcheolseung@gmail.com>
2022-06-13 23:27:01 +0900

    style: remove semicolon

--- ban.js
+++ ban.js
@@ -1,17 +1,17 @@
 const board = window.location.pathname.split('/')[2]
-let ban_dict = {};
+let ban_dict = {}
 
-load();
+load()
 
 function load() {
-    // chrome.storage.sync.clear();
+    // chrome.storage.sync.clear()
     chrome.storage.sync.get('seiyon_ban_list', ({ seiyon_ban_list }) => {
-        // console.log("items: " + JSON.stringify(items));
+        // console.log("items: " + JSON.stringify(items))
         if (seiyon_ban_list) {
             ban_dict = seiyon_ban_list
         }
-        changePage();
-    });
+        changePage()
+    })
 }
 
 
@@ -27,7 +27,7 @@ function changePage() {
             const date = datetime.toLocaleDateString()
 
             username.classList.add('button')
-            username.addEventListener("click", function(){handleButtonClick(date, username.title)});
+            username.addEventListener("click", function(){handleButtonClick(date, username.title)})
 
             // console.log(ban_dict)
             if (ban_dict
@@ -57,30 +57,30 @@ function handleButtonClick(date, title) {
 function pushBlockItem(date, id) {
     if (typeof ban_dict[board] == "undefined" || ban_dict[board] == null) { // 차단 목록 생성
         console.log(board + ' ban list 생성')
-        ban_dict[board] = {}; // 새로운 dict
+        ban_dict[board] = {} // 새로운 dict
     }
 
     if (typeof ban_dict[board][date] == "undefined" || ban_dict[board][date] == null) { // 차단 목록 생성
         console.log(board + ' ' + date + ' ban list 생성')
-        ban_dict[board][date] = new Array(); // 새로운 array
+        ban_dict[board][date] = new Array() // 새로운 array
     }
 
     const index = ban_dict[board][date].indexOf(id)
     if (index === -1) {
-        console.log(id + '차단');
+        console.log(id + '차단')
         ban_dict[board][date].push(id)
-        alert(id + '를 차단했습니다.');
-        console.log(ban_dict);
+        alert(id + '를 차단했습니다.')
+        console.log(ban_dict)
     } else {
         if (confirm(id  + '는 이미 차단한 아이디입니다. 차단을 해제하시겠습니까?')) {
             ban_dict[board][date].splice(index)
-            alert('차단을 해제했습니다.');
+            alert('차단을 해제했습니다.')
             if(ban_dict[board][date].length == 0) {
-                console.log('길이 0');
+                console.log('길이 0')
                 delete ban_dict[board][date]
             }
         }
     }
-    console.log(ban_dict);
-    chrome.storage.sync.set({'seiyon_ban_list': ban_dict});
+    console.log(ban_dict)
+    chrome.storage.sync.set({'seiyon_ban_list': ban_dict})
 }
